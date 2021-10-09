a3eb41a04b7e9e628c0c7ef0e2d01acc3854c0c6
DenilsonPaco98 <denilsonpaco36@gmail.com>
2021-10-09 15:04:06 -0300

    Day 01 python

---
+++ day1.py
@@ -0,0 +1,18 @@
+#pergunte onde nasceu 
+city = input("Olá estudante ! \nQual cidade voçê nasceu ?!")
+print(city + "...\nMuito bom eu morei por lá 5 anos é muito bom lá ...")
+
+#Qual é seu nome
+name = input("Qual é seu nome mesmo ?")
+
+print(name + ",\nEstamos recolhendo suas informações para uma pesquisa =) , coisa rapida ... ")
+#Qual o nome do animal de estimação 
+pet = input("Qual é o nome do seu animal de estimação ?")
+
+print("Anotado :" + pet)
+print("========================================")
+print("Resultado da pesquisa .... ")
+print("\n")
+
+print("Seu nome é :" + name + "\nNasceu em : " + city + "\nE teve seu animal de estimação chamado :" + pet)
+
